5caac51a9e83a437a5d91b6f9af9196495f6735a
Yitzi-boop <yreyes60889@ufide.ac.cr>
2025-10-10 19:16:20 -0600

    Initial commit

---
+++ OneDrive - Universidad Fidélitas/Proyecto/src/main/java/com/company/proyecto/Proyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.company.proyecto;
+
+/**
+ *
+ * @author nayel
+ */
+public class Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
